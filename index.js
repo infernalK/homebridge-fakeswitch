ef78af1761f4a6d64efe6e807773706460536afe
infernalK <59821935+infernalK@users.noreply.github.com>
2020-01-16 10:58:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Service, Characteristic;
 module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("switch-plugin", "MyAwesomeSwitch", mySwitch);
+  homebridge.registerAccessory("homebridge-FakeSwitch", "MyAwesomeSwitch", mySwitch);
 };
 
 mySwitch.prototype = {
